bf2ad35e6e961cdbdf5884152fd8c8f1c71c79b4
Sowmya <72663644+Sowmya-070@users.noreply.github.com>
2025-03-11 21:48:39 -0500

    Implemented password validation in login page

--- views/login.php
+++ views/login.php
@@ -1,4 +1,4 @@
-<?php include '../config/db.php';?>
+<?php include '../config/db.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,13 +15,23 @@
                     <h4 class="mb-3 f-w-400">Login</h4>
                     <form method="post" action="../controllers/auth.php">
                         <div class="input-group mb-3">
-                            <!-- <span class="input-group-text"><i data-feather="user"></i></span> -->
+                            <span class="input-group-text"><i data-feather="user"></i></span>
                             <input type="email" name="email" class="form-control" placeholder="Email" required>
                         </div>
-                        <div class="input-group mb-4">
+                        <!-- <div class="input-group mb-4">
                             <span class="input-group-text"><i data-feather="lock"></i></span>
                             <input type="password" name="password" class="form-control" placeholder="Password" required>
-                        </div>
+                        </div> -->
+                        <div class="input-group mb-4">
+    <span class="input-group-text"><i data-feather="lock"></i></span>
+    <input type="password" id="password" name="password" class="form-control"
+           placeholder="Password" required
+           pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"
+           title="Password must contain at least one uppercase letter, one lowercase letter, one number, one special character, and be at least 8 characters long.">
+</div>
+<small class="text-danger" id="passwordError" style="display:none;">Password must contain at least one uppercase, one lowercase, one number, one special character, and be at least 8 characters long.</small>
+
+
                         <div class="mb-3 mt-2">
                             <div class="form-check text-start">
                                 <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
