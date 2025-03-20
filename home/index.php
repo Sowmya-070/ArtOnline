2012e3f265d19710739e9652f33cdda29877c5c6
Sowmya <72663644+Sowmya-070@users.noreply.github.com>
2025-03-20 15:33:15 -0500

    modifed index.php to rectify  console errors

--- home/index.php
+++ home/index.php
@@ -47,8 +47,8 @@
         <!--responsive.css-->
         <link rel="stylesheet" href="assets/css/responsive.css">
 
-		<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<!-- <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script> -->
+			<!-- <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script> -->
         
         <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -98,8 +98,7 @@
 
 			<div class="container">
 				<div class="welcome-hero-txt">
-					<h2>Online Art Studio Using 
-						<br>Recommendation Algorithm
+					<h2>Online Art Studio 
 					</h2>
 				</div>
 			</div>
